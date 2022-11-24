c79ab05db0c5085dea892b543fd230162ed51b0a
Kamil <Kamil.Urbanik@o2.pl>
2022-11-24 20:20:56 +0100

    #0016 - add operations with function and form

--- js/components/Operations.js
+++ js/components/Operations.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from "./Button";
+import Operation from "./Operation";
+import {createOperation} from "../api/operations";
 
-const Operations = () => {
+const Operations = ({taskID, form, setForm, operations, setOperations, status}) => {
+    //operation Description
+    const [operation, setOperation] = useState("");
+
+    const newOperation = (e) => {
+        e.preventDefault();
+        const operationen = {
+            description: operation,
+            timeSpent: 0
+        }
+        createOperation(taskID, operationen, (data) => {
+            setOperations(prev => {
+                return [data, ...prev];
+            })
+        });
+        setForm(false);
+        setOperation("");
+    }
+
+    const removeOperation = (id) => {
+        setOperations(prev => prev.filter(operation => operation.id !== id));
+    }
+
     return (
         <>
+            {form && (
+                <div className="card-body">
+                    <form onSubmit={newOperation}>
+                        <div className="input-group">
+                            <input type="text"
+                                   className="form-control"
+                                   placeholder="Operation description"
+                                   value={operation}
+                                   onChange={e => setOperation(e.target.value)}/>
 
+                            <div className="input-group-append">
+                                <Button color={"info"} icon="fas fa-plus-circle">Add</Button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            )}
+            <ul className="list-group list-group-flush">
+                {operations.map(operation => (
+                    <Operation key={operation.id} {...operation} onRemoveOperation={removeOperation}
+                               status={status}/>
+                ))}
+            </ul>
         </>
     );
 };
