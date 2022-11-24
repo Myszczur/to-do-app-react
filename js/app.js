dca29e046618f9a47b7579981b4f04f356c28aea
Kamil <Kamil.Urbanik@o2.pl>
2022-11-24 19:03:29 +0100

    #0004 - add components

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return <h1>Hello World</h1>
+    const [task, setTask] = useState([]);
+
+    return <h1>Hello World</h1>
 }
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
---
+++ js/components/Operations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Operations = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Operations;
